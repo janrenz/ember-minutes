129a7b1c8d80673ec97dacc63505c4990f227d96
Jan Renz <jan.renz@gmail.com>
2015-03-26 18:35:18 +0100

    Added Detail view

--- app/router.js
+++ app/router.js
@@ -6,6 +6,9 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.resource('todo', {path: "/todo/:todo_id/"}, function() {
+    this.route('show');
+  });
 });
 
 export default Router;
---
+++ app/routes/todo/show.js
@@ -0,0 +1,11 @@
+
+
+import Ember from 'ember';
+export default Ember.Route.extend({
+    model: function(param, transition) {
+    return this.store.find('todo', transition.params.todo.todo_id);
+  },
+  setupController: function(controller, model) {
+    controller.set('model', model);
+  }
+});
