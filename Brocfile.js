291784c7ea8e6d16e90a59abaf726d203a81fca0
Jan Renz <jan.renz@gmail.com>
2015-03-26 16:45:31 +0100

    Added bootstrap via bower

--- Brocfile.js
+++ Brocfile.js
@@ -17,7 +17,8 @@ var app = new EmberApp();
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
-
+app.import('bower_components/bootstrap/dist/css/bootstrap.min.css');
+app.import('bower_components/bootstrap/dist/js/bootstrap.min.js');
 
 // Use vendor version instead of bower so we can make our custom modifications
 app.import('vendor/ember-hoodie-adapter/ember-hoodie-adapter.js');
