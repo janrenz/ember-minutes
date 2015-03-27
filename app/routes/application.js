91caedafc7388de49836e0fe41b4b64ce7fbc63b
Jan Renz <jan.renz@gmail.com>
2015-03-27 15:26:51 +0100

    Added sorting

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,7 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-    return this.store.find('todo');
+    return this.store.find('todo').then(sortByCompleted);
+    function sortByCompleted(items){
+      var sortProperties = ["completed","created_at"];
+      items = Ember.ArrayProxy.extend(Ember.SortableMixin).create(items);
+      items.set('sortProperties', sortProperties);
+      return items;
+    }
   },
 
   actions: {
