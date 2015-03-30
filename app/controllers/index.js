fa5e7939920fcb2a9cfe1dbb1d28fea1627303ad
Jan Renz <jan.renz@gmail.com>
2015-03-30 23:55:05 +0200

    Use sorted content in index view

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
-  sortProperties: ['completed', '_updatedAt'],
+  sortProperties: ['completed'],
   sortAscending: true,
   remaining: function () {
     return this.filterBy('completed', false).get('length');
